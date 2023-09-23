6fb3e7662814ba5ce94420e819039f1b6bd4fde4
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-22 20:29:36 -0700

    Loop through each char in userInput and implement IsLetterOrDigit- increment each letter to cleanString

--- Palindrome/Models/Palindrome.cs
+++ Palindrome/Models/Palindrome.cs
@@ -7,7 +7,14 @@ namespace Palindromes.Models
     public static bool IsPalindrome(string userInput)
     {
       string cleanString = "";
-      char[] palindromeCheckArray = userInput.ToCharArray();
+      foreach (char letter in cleanString)
+      {
+        if (char.IsLetterOrDigit(letter))
+        {
+          cleanString += char.ToLower(letter);
+        }
+      }
+      char[] palindromeCheckArray = cleanString.ToCharArray();
       Array.Reverse(palindromeCheckArray);
       for (int i = 0; i < palindromeCheckArray.Length; i++)
       {
